169acfb27bc7f0c23a0b018ad69b23d692a7e865
Sumit-2472 <sumitkalia2472@gmail.com>
2026-07-07 21:28:41 +0530

    src/auth.controller.js issue fix

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,40 +1,48 @@
 
 import User from "../models/user.model.js";
 import genToken from "../config/token.js";
-export const googleAuth=async(req,res)=>{
-    try{
-        const {email,name}=req.body; // frontend se data lenge
-        let user=await User.findOne({email}); 
-        if(!user){
-            user=await User.create({email,name}); // data se user create krnege
-        }
-        const token = genToken(user._id);
-        console.log("TOKEN:", token);
-        console.log("TYPE:", typeof token);
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: none,
-            path:"/"
-        });
-        return res.status(200).json({
-            message: "User authenticated successfully",
-            user: {
-              _id: user._id,
-              name: user.name,
-              email: user.email,
-              credits: user.credits,
-            },
-            token: token
-        });
+export const googleAuth = async (req, res) => {
+  try {
+    console.log("BODY:", req.body);
+
+    const { email, name } = req.body;
+    console.log("EMAIL:", email);
+    console.log("NAME:", name);
+
+    let user = await User.findOne({ email });
+    console.log("USER FOUND:", user);
+
+    if (!user) {
+      user = await User.create({ email, name });
+      console.log("USER CREATED:", user);
     }
-    catch(err){
-        console.log(err,"Error in google auth");
-        return res.status(500).json({
-            message: "Internal Server Error"
-        });
-    }
-}
+
+    console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
+
+    const token = genToken(user._id);
+    console.log("TOKEN:", token);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",   // <-- fix this
+      path: "/",
+    });
+
+    return res.status(200).json({
+      message: "User authenticated successfully",
+      user,
+      token,
+    });
+  } catch (err) {
+    console.error("GOOGLE AUTH ERROR:", err);
+    console.error(err.stack);
+
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 
 
